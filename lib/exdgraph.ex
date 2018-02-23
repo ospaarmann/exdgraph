421cc681e6024e4d4a733ba0432951afa2f63bfb
Ole Spaarmann <os@ospaarmann.com>
2018-02-23 16:27:01 +0100

    Initial commit

---
+++ lib/exdgraph.ex
@@ -0,0 +1,18 @@
+defmodule Exdgraph do
+  @moduledoc """
+  Documentation for Exdgraph.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exdgraph.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exdgraph_test.exs
@@ -0,0 +1,8 @@
+defmodule ExdgraphTest do
+  use ExUnit.Case
+  doctest Exdgraph
+
+  test "greets the world" do
+    assert Exdgraph.hello() == :world
+  end
+end
