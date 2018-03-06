a23e8a2412c78b58e615dda8c6be528bd93f5c57
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2018-03-07 00:21:42 +0100

    Add basic configs for dev/test with default values.

---
+++ config/dev.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+config :ex_dgraph, ExDgraph,
+  url: 'localhost', # default port considered to be: 9080
+  pool_size: 5,
+  max_overflow: 1,
+  # retry the request, in case of error - in the example below the retry will
+  # linearly increase the delay from 150ms following a Fibonacci pattern,
+  # cap the delay at 15 seconds (the value defined by the default `:timeout`
+  # parameter) and giving up after 3 attempts
+  retry_linear_backoff: [delay: 150, factor: 2, tries: 3]
+  # the `retry_linear_backoff` values above are also the default driver values,
+  # re-defined here mostly as a reminder
