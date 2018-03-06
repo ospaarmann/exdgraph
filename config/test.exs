f551dff18e9a2a4707c23deab13b7c7cf4b68031
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2018-03-07 00:21:42 +0100

    Add basic configs for dev/test with default values.

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
+<<<<<<< HEAD
 # Application.put_env(:elixir, :ansi_enabled, true)
 config :elixir, ansi_enabled: true
 
@@ -35,3 +36,16 @@ config :logger, :console,
 config :exdgraph,
   dgraphServerHTTP: "http://localhost:8082",
   dgraphServerGRPC: "localhost:9080"
+=======
+config :ex_dgraph, ExDgraph,
+  url: 'localhost', # default port considered to be: 9080
+  pool_size: 5,
+  max_overflow: 1,
+  # retry the request, in case of error - in the example below the retry will
+  # linearly increase the delay from 150ms following a Fibonacci pattern,
+  # cap the delay at 15 seconds (the value defined by the default `:timeout`
+  # parameter) and giving up after 3 attempts
+  retry_linear_backoff: [delay: 150, factor: 2, tries: 3]
+  # the `retry_linear_backoff` values above are also the default driver values,
+  # re-defined here mostly as a reminder
+>>>>>>> Add basic configs for dev/test with default values.
