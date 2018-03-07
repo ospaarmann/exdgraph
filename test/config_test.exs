65ee06935c905945cf984d6bbab9dddfed5a3f22
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2018-03-07 01:46:50 +0100

    Add tests for standard configuration.

---
+++ test/config_test.exs
@@ -0,0 +1,28 @@
+defmodule Config.Test do
+  use ExUnit.Case
+  alias ExDgraph.Utils
+
+  @basic_config [
+    hostname: 'ole',
+    port: 1234,
+    pool_size: 10,
+    max_overflow: 7
+  ]
+
+  test "standard Bolt.Sips configuration parameters" do
+    config = Utils.default_config(@basic_config)
+
+    assert config[:hostname] == 'ole'
+    assert config[:port] == 1234
+    assert config[:pool_size] == 10
+    assert config[:max_overflow] == 7
+  end
+
+  test "standard ExDgraph default configuration" do
+    config = Utils.default_config([])
+
+    assert config[:hostname] == 'localhost'
+    assert config[:port] == 9080
+  end
+
+end
