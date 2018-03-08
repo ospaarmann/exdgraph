bc3f97ae3053a9cd5d68a3fe740010be33256386
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2018-03-08 22:46:21 +0100

    Add test for query/2

---
+++ test/query_test.exs
@@ -0,0 +1,46 @@
+defmodule ExDgraph.QueryTest do
+  use ExUnit.Case, async: true
+  import ExDgraph.TestHelper
+
+  @sample_query """
+    {
+        starwars(func: anyofterms(name, "VI"))
+        {
+          uid
+          name
+          release_date
+          starring
+          {
+            name
+          }
+        }
+    }
+  """
+
+  setup_all do
+    conn = ExDgraph.conn()
+    drop_all()
+    import_starwars_sample()
+
+    on_exit(fn ->
+      # close channel ?
+      :ok
+    end)
+
+    [conn: conn]
+  end
+
+  test "query/2 with correct query returns {:ok, query_msg}", %{conn: conn} do
+    {:ok, query_msg} = ExDgraph.Query.query(conn, @sample_query)
+    res = query_msg.result
+    starwars = res["starwars"]
+    one = List.first(starwars)
+    assert "Star Wars: Episode VI - Return of the Jedi" == one["name"]
+    assert "1983-05-25" == one["release_date"]
+  end
+
+  test "query/2 with wrong query returns {:error, error}", %{conn: conn} do
+    res = ExDgraph.Query.query(conn, "wrong")
+    assert res == {:error, [code: 2, message: "while lexing wrong: Invalid operation type: wrong"]}
+  end
+end
