671dd12493da7dc6b2e3a6b1e9171743f6c8c5ac
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2018-03-08 01:15:10 +0100

    Start supervisor tree for tests.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,11 @@
-ExUnit.start()
+# Logger.configure(level: :info)
+ExUnit.start(exclude: [:skip])
+
+defmodule ExDgraph.TestHelper do
+end
+
+if Process.whereis(ExDgraph.pool_name()) == nil do
+  {:ok, _pid} = ExDgraph.start_link(Application.get_env(:ex_dgraph, ExDgraph))
+end
+
+Process.flag(:trap_exit, true)
