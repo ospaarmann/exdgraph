914e844ad94894cdf0d123e87e31353e3597e9d5
Eric Hagman <emhagman@gmail.com>
2018-08-30 13:02:25 -0400

    fix(protocol): handle :gun_up and :gun_down as debug level for now

--- lib/exdgraph/protocol.ex
+++ lib/exdgraph/protocol.ex
@@ -87,6 +87,22 @@ defmodule ExDgraph.Protocol do
     end
   end
 
+  def handle_info({:gun_up, _pid, _protocol}) do
+    Logger.debug(fn ->
+      [inspect(__MODULE__), ?\s, inspect(self()), " received gun_up from server"]
+    end)
+
+     {:ok, state}
+  end
+
+  def handle_info({:gun_down, _pid, _protocol, _level, _, _}) do
+    Logger.debug(fn ->
+      [inspect(__MODULE__), ?\s, inspect(self()), " received gun_down from server"]
+    end)
+
+    {:ok, state}
+  end
+
   def handle_info(msg, state) do
     Logger.error(fn ->
       [inspect(__MODULE__), ?\s, inspect(self()), " received unexpected message: " | inspect(msg)]
