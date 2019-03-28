dc5f2490ce7cbc4d4acb732633b1a01e1da8cd51
Ole Spaarmann <os@ospaarmann.com>
2019-03-28 11:10:16 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExDgraph.MixProject do
   def project do
     [
       app: :ex_dgraph,
-      version: "0.2.0-beta.1",
+      version: "0.2.0-beta.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
