c943253a17732bdea53671cfde50322184455b66
Ole Spaarmann <os@ospaarmann.com>
2019-09-05 23:25:01 +0200

    Test connection errors.

--- test/exdgraph_test.exs
+++ test/exdgraph_test.exs
@@ -1,3 +1,22 @@
 defmodule ExDgraphTest do
   use ExUnit.Case
+  import ExUnit.CaptureLog
+
+  test "connection_errors" do
+    Process.flag(:trap_exit, true)
+    opts = [backoff_type: :stop, max_restarts: 0, timeout: 500]
+
+    assert capture_log(fn ->
+             {:ok, pid} = ExDgraph.start_link([hostname: "non_existing"] ++ opts)
+             assert_receive {:EXIT, ^pid, :killed}, 10000
+           end) =~
+             "** (ExDgraph.Error) connect failed with \"Error when opening connection: :timeout\""
+
+    assert capture_log(fn ->
+             {:ok, pid} = ExDgraph.start_link([port: 700] ++ opts)
+
+             assert_receive {:EXIT, ^pid, :killed}, 10000
+           end) =~
+             "** (ExDgraph.Error) connect failed with \"Error when opening connection: :timeout\""
+  end
 end
