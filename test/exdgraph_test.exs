e01ad16f2b82cc00a112c5e313a53d00c5fa753a
Ole Spaarmann <os@ospaarmann.com>
2019-09-06 17:54:50 +0200

    Move query tests to separate file.

--- test/exdgraph_test.exs
+++ test/exdgraph_test.exs
@@ -3,16 +3,6 @@ defmodule ExDgraphTest do
   import ExDgraph.TestHelper
   import ExUnit.CaptureLog
 
-  alias ExDgraph.{Error, Query, Result}
-
-  setup_all do
-    {:ok, conn} = ExDgraph.start_link()
-    ExDgraph.operation(conn, %{drop_all: true})
-    import_starwars_sample(conn)
-
-    [conn: conn]
-  end
-
   describe "start_link/1" do
     test "it starts the client" do
       {status, pid} = ExDgraph.start_link()
@@ -47,58 +37,6 @@ defmodule ExDgraphTest do
                assert_receive {:EXIT, ^pid, :killed}, 10000
              end) =~
                "** (ExDgraph.Error) connect failed with \"Error when opening connection: :timeout\""
-    end
-  end
-
-  describe "Query" do
-    @sample_query """
-      {
-          starwars(func: anyofterms(name, "VI"))
-          {
-            uid
-            name
-            release_date
-            starring
-            {
-              name
-            }
-          }
-      }
-    """
-
-    test "query/3 with correct query returns {:ok, %Query{}, %Result{}}", %{conn: conn} do
-      {status, %Query{} = _query, %Result{} = result} = ExDgraph.query(conn, @sample_query)
-      assert status == :ok
-      data = result.data
-      starwars = List.first(data.starwars)
-      assert starwars.name == "Star Wars: Episode VI - Return of the Jedi"
-      assert starwars.release_date == "1983-05-25"
-      assert length(starwars.starring) == 3
-    end
-
-    test "query/3 with wrong query returns {:error, %Error{}}", %{conn: conn} do
-      {status, %Error{} = error} = ExDgraph.query(conn, "wrong")
-      assert status == :error
-
-      assert %Error{} = error
-      assert error.code == 2
-      assert error.action == :query
-      assert error.reason =~ "Invalid operation type: wrong"
-    end
-
-    test "query!/2 with correct query returns query_msg", %{conn: conn} do
-      %Result{} = result = ExDgraph.query!(conn, @sample_query)
-      data = result.data
-      starwars = List.first(data.starwars)
-      assert starwars.name == "Star Wars: Episode VI - Return of the Jedi"
-      assert starwars.release_date == "1983-05-25"
-      assert length(starwars.starring) == 3
-    end
-
-    test "query!/2 raises ExDgraph.Exception", %{conn: conn} do
-      assert_raise ExDgraph.Error, fn ->
-        ExDgraph.query!(conn, "wrong")
-      end
     end
   end
 end
---
+++ test/query_test.exs
@@ -0,0 +1,65 @@
+defmodule ExDgraph.QueryTest do
+  @moduledoc false
+
+  use ExUnit.Case
+  import ExDgraph.TestHelper
+  alias ExDgraph.{Error, Query, Result}
+
+  @sample_query """
+    {
+        starwars(func: anyofterms(name, "VI"))
+        {
+          uid
+          name
+          release_date
+          starring
+          {
+            name
+          }
+        }
+    }
+  """
+
+  setup_all do
+    {:ok, conn} = ExDgraph.start_link()
+    ExDgraph.alter(conn, %{drop_all: true})
+    import_starwars_sample(conn)
+
+    [conn: conn]
+  end
+
+  test "query/3 with correct query returns {:ok, %Query{}, %Result{}}", %{conn: conn} do
+    {status, %Query{} = _query, %Result{} = result} = ExDgraph.query(conn, @sample_query)
+    assert status == :ok
+    data = result.data
+    starwars = List.first(data.starwars)
+    assert starwars.name == "Star Wars: Episode VI - Return of the Jedi"
+    assert starwars.release_date == "1983-05-25"
+    assert length(starwars.starring) == 3
+  end
+
+  test "query/3 with wrong query returns {:error, %Error{}}", %{conn: conn} do
+    {status, %Error{} = error} = ExDgraph.query(conn, "wrong")
+    assert status == :error
+
+    assert %Error{} = error
+    assert error.code == 2
+    assert error.action == :query
+    assert error.reason =~ "Invalid operation type: wrong"
+  end
+
+  test "query!/2 with correct query returns query_msg", %{conn: conn} do
+    %Result{} = result = ExDgraph.query!(conn, @sample_query)
+    data = result.data
+    starwars = List.first(data.starwars)
+    assert starwars.name == "Star Wars: Episode VI - Return of the Jedi"
+    assert starwars.release_date == "1983-05-25"
+    assert length(starwars.starring) == 3
+  end
+
+  test "query!/2 raises ExDgraph.Exception", %{conn: conn} do
+    assert_raise ExDgraph.Error, fn ->
+      ExDgraph.query!(conn, "wrong")
+    end
+  end
+end
